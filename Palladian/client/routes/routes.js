3caf8277c9920b83fda98e24d88c89d6b9e590e0
JoeMarion <joeyzmarion@gmail.com>
2016-01-26 20:17:37 -0600

    Add stock photos

--- Palladian/client/routes/routes.js
+++ Palladian/client/routes/routes.js
@@ -6,10 +6,30 @@ Router.route('/', function(){
   this.render('Dashboard');
 });
 
-Router.route('/register/', function() {
-  this.render('Register')
-});
-
 Router.route('/services/', function() {
   this.render ('Services');
+});
+
+Router.route('/massage/', function() {
+  this.render ('Massage')
+});
+
+Router.route('/hair/', function() {
+  this.render ('Hair')
+});
+
+Router.route('/waxing/', function() {
+  this.render ('Waxing')
+});
+
+Router.route('/tanning/', function() {
+  this.render ('Tanning')
+});
+
+Router.route('/skincare/', function() {
+  this.render ('SkinCare')
+});
+
+Router.route('/nails/', function() {
+  this.render ('Nails')
 });
